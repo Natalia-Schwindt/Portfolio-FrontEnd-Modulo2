2b2e6d596e190cd104b6e5f88e4ed6d8620095d3
Natalia Schwindt <naty8014@gmail.com>
2022-08-04 08:58:29 -0300

    primer commit

---
+++ Json.js
@@ -0,0 +1,26 @@
+[
+    {
+        "datos personales":  {
+        "nombre": "Natalia",
+        "apellido": "Schwindt",
+        "edad": 41,
+        "nacionalidad": "Argentina"
+        },
+    
+        "datos laborales": {
+        "1": "1"
+        },
+        "estudios": {
+        "1": "1"
+        },
+        "habilidades duras": {
+        "1": "1"
+        },
+        "habilidades blandas": {
+        "1": "1"
+        },
+        "proyectos":{
+        "1": "1"
+        }
+    }
+]
